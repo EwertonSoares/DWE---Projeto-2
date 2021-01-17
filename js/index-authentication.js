24de5b3e62ea42556725dfdcca723f50a2181c56
Ewerton Soares Da Silva <CIANDT\ewerton@lnb020032cps.ciandt.global>
2021-01-17 19:51:30 -0300

    Iniciando dsiner da Pagina inicial

--- js/index-authentication.js
+++ js/index-authentication.js
@@ -1,16 +1,16 @@
-const sair = document.getElementById("logout");
+// const sair = document.getElementById("logout");
 
-sair.addEventListener("click", function logout() {
-    firebase.auth().signOut()
-        .then(function () {
+// sair.addEventListener("click", function logout() {
+//     firebase.auth().signOut()
+//         .then(function () {
             
-            localStorage.setItem("acesso", false);
-            localStorage.removeItem("userEmail");
-            localStorage.removeItem("userUid");
+//             localStorage.setItem("acesso", false);
+//             localStorage.removeItem("userEmail");
+//             localStorage.removeItem("userUid");
 
-            window.location.href = "login.html";
+//             window.location.href = "login.html";
         
-        }).catch(function () {
+//         }).catch(function () {
 
-        });
-});
+//         });
+// });
